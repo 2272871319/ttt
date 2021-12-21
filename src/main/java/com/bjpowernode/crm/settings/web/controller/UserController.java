d4d7d1bc10fbe7b4204606631ba03c35f828afd6
dx <dx@126.com>
2021-12-21 17:47:22 +0800

    1.写了简单登录 2.安装mybatis逆向工程 自动生成了实体类

--- src/main/java/com/bjpowernode/crm/settings/web/controller/UserController.java
+++ src/main/java/com/bjpowernode/crm/settings/web/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.bjpowernode.crm.settings.web.controller;
 
+import com.bjpowernode.crm.commons.utils.MD5Util;
+import com.bjpowernode.crm.settings.mapper.UserMapper;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * ClassName:
@@ -14,5 +22,15 @@ public class UserController {
     @RequestMapping("/settings/qx/user/toLogin.do")
     public String toLogin() {
         return "settings/qx/user/login";
+    }
+
+    @RequestMapping("/settings/qx/user/login.do")
+    public @ResponseBody Object login(String loginAct,String loginPwd){
+        Map<String,Object> paraMap = new HashMap<>();
+        paraMap.put("loginAct",loginAct);
+        paraMap.put("loginPwd", MD5Util.getMD5(loginPwd));
+
+        
+        return null;
     }
 }
